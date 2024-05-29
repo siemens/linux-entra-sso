378fb50d53e2a7953b6da296ba1e3f8128c30633
Felix Moessbauer <felix.moessbauer@siemens.com>
2024-05-29 14:18:54 +0200

    refactor: modularize sso_mib script

--- sso_mib.py
+++ sso_mib.py
@@ -7,81 +7,92 @@ from pydbus import SessionBus
 import uuid
 
 
-def acquirePrtSsoCookie(broker):
-    """
-    Acquire a Prt SSO cookie from the broker.
-    """
-    context = {
-        'clientId': str(session_id),
-        'redirectUri': str(session_id)
-    }
-    resp = json.loads(broker.getAccounts('0.0',
-                                         str(session_id),
-                                         json.dumps(context)))
+class NativeMessaging:
+    @staticmethod
+    def getMessage():
+        """
+            Read a message from stdin and decode it.
+        """
+        rawLength = sys.stdin.buffer.read(4)
+        if len(rawLength) == 0:
+            sys.exit(0)
+        messageLength = struct.unpack('@I', rawLength)[0]
+        message = sys.stdin.buffer.read(messageLength).decode('utf-8')
+        return json.loads(message)
 
-    account = resp['accounts'][0]
-    tenant = account['realm']
-    request = {
-        'account': account,
-        'authParameters': {
-            'accessTokenToRenew': '',
+    @staticmethod
+    def encodeMessage(messageContent):
+        """
+            Encode a message for transmission, given its content
+        """
+        encodedContent = json.dumps(messageContent, separators=(',', ':')) \
+            .encode('utf-8')
+        encodedLength = struct.pack('@I', len(encodedContent))
+        return {'length': encodedLength, 'content': encodedContent}
+
+    @staticmethod
+    def sendMessage(encodedMessage):
+        """
+            Send an encoded message to stdout
+        """
+        sys.stdout.buffer.write(encodedMessage['length'])
+        sys.stdout.buffer.write(encodedMessage['content'])
+        sys.stdout.buffer.flush()
+
+
+class SsoMib:
+    def __init__(self):
+        self.broker = SessionBus().get(
+            "com.microsoft.identity.broker1",
+            "/com/microsoft/identity/broker1")
+        self.session_id = uuid.uuid4()
+
+    def getAccounts(self):
+        context = {
+            'clientId': str(self.session_id),
+            'redirectUri': str(self.session_id)
+        }
+        resp = self.broker.getAccounts('0.0',
+                                       str(self.session_id),
+                                       json.dumps(context))
+        return json.loads(resp)['accounts']
+
+    def acquirePrtSsoCookie(self, account):
+        tenant = account['realm']
+        request = {
             'account': account,
-            'additionalQueryParametersForAuthorization': {},
-            'authority': 'https://login.microsoftonline.com/common',
-            'authorizationType': 8,  # OAUTH2
-            'clientId': str(session_id),
-            'decodedClaims': '',
-            'enrollmentId': '',
-            'password': '',
-            'popParams': None,
-            'redirectUri': 'https://login.microsoftonline.com/common/oauth2/nativeclient',
-            'requestedScopes': ["https://graph.microsoft.com/.default"],
-            'username': account['username'],
-            'uxContextHandle': -1
-            },
-        'ssoUrl': f'https://login.microsoftonline.com/{tenant}/oauth2/v2.0/authorize'
-    }
-    token = json.loads(broker.acquirePrtSsoCookie('0.0', str(session_id), json.dumps(request)))
-    return token
+            'authParameters': {
+                'accessTokenToRenew': '',
+                'account': account,
+                'additionalQueryParametersForAuthorization': {},
+                'authority': 'https://login.microsoftonline.com/common',
+                'authorizationType': 8,  # OAUTH2
+                'clientId': str(self.session_id),
+                'decodedClaims': '',
+                'enrollmentId': '',
+                'password': '',
+                'popParams': None,
+                'redirectUri': 'https://login.microsoftonline.com'
+                               '/common/oauth2/nativeclient',
+                'requestedScopes': ["https://graph.microsoft.com/.default"],
+                'username': account['username'],
+                'uxContextHandle': -1
+                },
+            'ssoUrl': 'https://login.microsoftonline.com'
+                      f'/{tenant}/oauth2/v2.0/authorize'
+        }
+        token = json.loads(self.broker.acquirePrtSsoCookie(
+            '0.0', str(self.session_id), json.dumps(request)))
+        return token
 
 
-def getMessage():
-    """
-        Read a message from stdin and decode it.
-    """
-    rawLength = sys.stdin.buffer.read(4)
-    if len(rawLength) == 0:
-        sys.exit(0)
-    messageLength = struct.unpack('@I', rawLength)[0]
-    message = sys.stdin.buffer.read(messageLength).decode('utf-8')
-    return json.loads(message)
+if __name__ == '__main__':
+    ssomib = SsoMib()
 
-
-def encodeMessage(messageContent):
-    """
-        Encode a message for transmission, given its content
-    """
-    encodedContent = json.dumps(messageContent, separators=(',', ':')).encode('utf-8')
-    encodedLength = struct.pack('@I', len(encodedContent))
-    return {'length': encodedLength, 'content': encodedContent}
-
-
-def sendMessage(encodedMessage):
-    """
-        Send an encoded message to stdout
-    """
-    sys.stdout.buffer.write(encodedMessage['length'])
-    sys.stdout.buffer.write(encodedMessage['content'])
-    sys.stdout.buffer.flush()
-
-
-session_id = uuid.uuid4()
-bus = SessionBus()
-broker = bus.get(
-        "com.microsoft.identity.broker1",
-        "/com/microsoft/identity/broker1")
-
-while True:
-    receivedMessage = getMessage()
-    if receivedMessage == "acquirePrtSsoCookie":
-        sendMessage(encodeMessage(acquirePrtSsoCookie(broker)))
+    while True:
+        receivedMessage = NativeMessaging.getMessage()
+        if receivedMessage == "acquirePrtSsoCookie":
+            accounts = ssomib.getAccounts()
+            NativeMessaging.sendMessage(
+                NativeMessaging.encodeMessage(
+                    ssomib.acquirePrtSsoCookie(accounts[0])))
