84b8c2a1660baf8e672e44bc14783289a9c92e60
Felix Moessbauer <felix.moessbauer@siemens.com>
2025-07-16 12:27:01 +0200

    feat: support transparent token refresh on well-known apps
    On Firefox, we use the WebRequest API to inject the PRT SSO Cookie into
    the requests hitting the login provider. As the `WebRequest` API only operates
    on allowed URLs, applications that perform an automated token refresh in the
    background (e.g. the Teams PWA) also need to be listed. Otherwise the
    transparent re-login will fail (it misses the `PRT SSO Cookie`) and a manual
    re-login will be required. The same applies to the declarativeNetRequest API
    on chromium based browsers. As we now filter based on request type, we drop
    the nearly redundant request access header type filter.
    
    To require only the absolute minimum set of permissions, we do this on a
    case-by-case decision instead of whitelisting all URLs. We further
    update the privacy statement to include a reasoning. This commit just
    prepares the infrastructure. No apps are added.
    
    As we now rely on optional_host_permissions, we need to lift the minimal
    required Firefox version to 128. This is anyways the current ESR
    version, so we do not expect older versions in the field.
    
    Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

--- popup/menu.js
+++ popup/menu.js
@@ -8,6 +8,10 @@ let bg_port = chrome.runtime.connect({ name: "linux-entra-sso" });
 let inflight = false;
 /* user is logged in */
 let active = false;
+/* sso provider url */
+let sso_url = null;
+/* current URL filter */
+let current_filter = null;
 
 function set_inflight() {
     if (inflight) return false;
@@ -24,6 +28,7 @@ function clear_inflight() {
 bg_port.onMessage.addListener(async (m) => {
     if (m.event == "stateChanged") {
         clear_inflight();
+        check_bg_sso_enabled();
         if (m.account !== null) {
             document.getElementById("me-name").innerText = m.account.name;
             document.getElementById("me-email").innerText = m.account.username;
@@ -75,6 +80,8 @@ bg_port.onMessage.addListener(async (m) => {
             }
             document.getElementById("version").innerText = vstr;
         }
+        sso_url = m.sso_url;
+        check_sso_provider_perms();
     }
 });
 
@@ -88,3 +95,85 @@ document.getElementById("entity-guest").addEventListener("click", (event) => {
     if (!set_inflight(this)) return;
     bg_port.postMessage({ command: "disable" });
 });
+
+function check_sso_provider_perms() {
+    msgbox = document.getElementById("message-box");
+    msgtext = document.getElementById("message-text");
+    grant_access_text = document.getElementById("grant-access-sso");
+    const permissionsToCheck = {
+        origins: [sso_url + "/*"],
+    };
+    chrome.permissions.contains(permissionsToCheck).then((result) => {
+        if (result) {
+            msgbox.classList.add("hidden");
+            msgbox.innerText = "";
+        } else {
+            msgtext.innerText = "No permission to access login provider.";
+            msgbox.classList.remove("hidden");
+        }
+    });
+}
+
+async function check_bg_sso_enabled() {
+    bg_sso_classes = document.getElementById("bg-sso-state").classList;
+    let [tab] = await chrome.tabs.query({ currentWindow: true, active: true });
+    if (!Object.hasOwn(tab, "url") || !tab.url.startsWith("https://")) {
+        bg_sso_classes.add("hidden");
+        return;
+    }
+    bg_sso_classes.remove("hidden");
+    var tab_hostname = new URL(tab.url).hostname;
+    current_filter = "https://" + tab_hostname + "/*";
+    document.getElementById("current-url").innerText = tab_hostname;
+    const permissionsToCheck = {
+        origins: [current_filter],
+    };
+    chrome.permissions.contains(permissionsToCheck).then((result) => {
+        var sso_state_classes =
+            document.getElementById("bg-sso-state").classList;
+        if (result) {
+            sso_state_classes.replace("disconnected", "connected");
+        } else {
+            sso_state_classes.replace("connected", "disconnected");
+        }
+    });
+}
+
+function request_host_permission(urls) {
+    if (urls === null || urls.length == 0) return;
+    const permissionsToRequest = {
+        origins: urls,
+    };
+    chrome.permissions.request(permissionsToRequest).then((granted) => {
+        if (granted) {
+            console.log("Permission granted");
+            // No need to update the UI as this will trigger the permission
+            // changed event in the background script, which triggers an
+            // UI update.
+        } else {
+            console.log("Failed to get permission");
+        }
+    });
+    // The permission-request window might open below the webextensions panel.
+    // This has been observed on Thunderbird 128. Close the panel, so the user
+    // can grant the permission.
+    window.close();
+}
+
+// Requires user interaction, as otherwise we lack the permission to
+// request further host permissions
+document.getElementById("grant-access").addEventListener("click", (event) => {
+    request_host_permission([current_filter]);
+});
+
+document
+    .getElementById("withdraw-access")
+    .addEventListener("click", (event) => {
+        remove_host_permission([current_filter]);
+    });
+
+document
+    .getElementById("grant-access-sso")
+    .addEventListener("click", (event) => {
+        request_host_permission([sso_url + "/*"]);
+    });
