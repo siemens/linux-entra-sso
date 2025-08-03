2f61e6f3a53d5018326fabcf18772f38c742773f
Felix Moessbauer <felix.moessbauer@siemens.com>
2025-08-03 18:00:33 +0200

    refactor(accounts): move account handling logic to class
    We introduce the AccountManager class to handle all account related
    tasks (like getting the accounts from the broker). By that, we can
    reduce the global state and also ensure a proper startup order and
    execution of the async account related callbacks.
    
    Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

--- src/utils.js
+++ src/utils.js
@@ -11,6 +11,20 @@ export function ssoLogError(message) {
     console.error("[Linux Entra SSO] " + message);
 }
 
+export async function load_icon(path, width) {
+    const response = await fetch(chrome.runtime.getURL(path));
+    let imgBitmap = await createImageBitmap(await response.blob(), {
+        resizeWidth: width,
+        resizeHeight: width,
+    });
+    let canvas = new OffscreenCanvas(width, width);
+    let ctx = canvas.getContext("2d");
+    ctx.save();
+    ctx.drawImage(imgBitmap, 0, 0);
+    ctx.restore();
+    return ctx.getImageData(0, 0, width, width);
+}
+
 /**
  * Promise that can externally be resolved or rejected.
  */
