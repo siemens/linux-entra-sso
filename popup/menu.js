3741a4a6a583bf9d9e50970634bbd2d43648d7eb
Felix Moessbauer <felix.moessbauer@siemens.com>
2025-08-22 09:32:30 +0200

    refactor(ui): unify state display logic
    Depending on the application state, UI elements are displayed or hidden.
    This is currently done for each element individually and with open-coded
    logic. We refactor this in the following way:
    
    - move html element annotations to functions (instead of open coding)
    - only work with positive states (if positive state is missing, we
      assume the state is negative)
    - move all static inline texts from JS to HTML
    
    Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

--- popup/menu.js
+++ popup/menu.js
@@ -18,18 +18,25 @@ let gpo = null;
 function set_inflight() {
     if (inflight) return false;
     inflight = true;
-    document.body.classList.add("pending");
+    annotate_body_if("pending", true);
     return true;
 }
 
 function clear_inflight() {
     inflight = false;
-    document.body.classList.remove("pending");
+    annotate_body_if("pending", false);
 }
 
 function annotate_body_if(annotation, state) {
     if (state) document.body.classList.add(annotation);
     else document.body.classList.remove(annotation);
+}
+
+function annotate_by_id_if(element_id, annotation, state) {
+    element = document.getElementById(element_id);
+    if (!element) return;
+    if (state) element.classList.add(annotation);
+    else element.classList.remove(annotation);
 }
 
 bg_port.onMessage.addListener(async (m) => {
@@ -42,7 +49,6 @@ bg_port.onMessage.addListener(async (m) => {
             document.getElementById("me-name").innerText = m.account.name;
             document.getElementById("me-email").innerText = m.account.username;
             const canvas = document.getElementById("me-avatar");
-            const fallback = document.getElementById("me-avatar-fallback");
             const ctx = canvas.getContext("2d");
             if (m.account.avatar !== null) {
                 let img = new Image();
@@ -51,34 +57,18 @@ bg_port.onMessage.addListener(async (m) => {
                     (img.src = m.account.avatar),
                 );
                 ctx.drawImage(img, 0, 0);
-                canvas.classList.remove("hidden");
-                fallback.classList.add("hidden");
-            } else {
-                canvas.classList.add("hidden");
-                fallback.classList.remove("hidden");
             }
+            annotate_by_id_if("me-avatar-fallback", "hidden", m.account.avatar);
+            annotate_by_id_if("me-avatar", "hidden", !m.account.avatar);
         }
-        if (m.enabled && m.account !== null) {
-            document.getElementById("entity-me").classList.add("active");
-            document.getElementById("entity-guest").classList.remove("active");
-            active = true;
-        } else {
-            document.getElementById("entity-me").classList.remove("active");
-            document.getElementById("entity-guest").classList.add("active");
-            active = false;
-        }
-        let broker_state_classes =
-            document.getElementById("broker-state").classList;
-        let broker_state_value = document.getElementById("broker-state-value");
-        if (m.broker_online) {
-            broker_state_classes.add("connected");
-            broker_state_classes.remove("disconnected");
-            broker_state_value.innerText = "connected";
-        } else {
-            broker_state_classes.remove("connected");
-            broker_state_classes.add("disconnected");
-            broker_state_value.innerText = "disconnected";
-        }
+
+        active = m.enabled && m.account !== null;
+        annotate_by_id_if("entity-me", "active", active);
+        annotate_by_id_if("entity-guest", "active", !active);
+
+        annotate_by_id_if("broker-state", "connected", m.broker_online);
+        document.getElementById("broker-state-value").innerText =
+            m.broker_online ? "connected" : "disconnected";
         document.getElementById("broker-version").innerText = m.broker_version;
 
         if (m.host_version) {
@@ -109,62 +99,38 @@ document.getElementById("entity-guest").addEventListener("click", (event) => {
 });
 
 function check_sso_provider_perms() {
-    msgbox = document.getElementById("message-box");
-    msgtext = document.getElementById("message-text");
-    grant_access_text = document.getElementById("grant-access-sso");
     const permissionsToCheck = {
         origins: [sso_url + "/*"],
     };
     chrome.permissions.contains(permissionsToCheck).then((result) => {
-        if (result) {
-            msgbox.classList.add("hidden");
-            msgbox.innerText = "";
-        } else {
-            msgtext.innerText = "No permission to access login provider.";
-            msgbox.classList.remove("hidden");
-        }
+        annotate_by_id_if("message-box", "hidden", result);
     });
 }
 
 async function check_bg_sso_enabled() {
-    bg_sso_classes = document.getElementById("bg-sso-state").classList;
     let [tab] = await chrome.tabs.query({ currentWindow: true, active: true });
     if (!Object.hasOwn(tab, "url") || !tab.url.startsWith("https://")) {
-        bg_sso_classes.add("hidden");
+        annotate_by_id_if("bg-sso-state", "hidden", true);
         return;
     }
-    bg_sso_classes.remove("hidden");
+    annotate_by_id_if("bg-sso-state", "hidden", false);
+
     var tab_hostname = new URL(tab.url).hostname;
     current_filter = "https://" + tab_hostname + "/*";
     document.getElementById("current-url").innerText = tab_hostname;
     const permissionsToCheck = {
         origins: [current_filter],
     };
-    var sso_state_classes = document.getElementById("bg-sso-state").classList;
     chrome.permissions.contains(permissionsToCheck).then((result) => {
-        if (result) {
-            sso_state_classes.replace("disconnected", "connected");
-        } else {
-            sso_state_classes.replace("connected", "disconnected");
-        }
+        annotate_by_id_if("bg-sso-state", "connected", result);
     });
-    if (
-        gpo !== null &&
-        (gpo.has_catch_all || tab_hostname in gpo.apps_managed)
-    ) {
-        sso_state_classes.add("immutable");
-    } else {
-        sso_state_classes.remove("immutable");
-    }
+    const state_immutable =
+        gpo !== null && (gpo.has_catch_all || tab_hostname in gpo.apps_managed);
+    annotate_by_id_if("bg-sso-state", "immutable", state_immutable);
 }
 
 function check_gpo_update() {
-    gpo_box_classes = document.getElementById("gpo-update-box").classList;
-    if (gpo === null || !gpo.pending) {
-        gpo_box_classes.add("hidden");
-        return;
-    }
-    gpo_box_classes.remove("hidden");
+    annotate_by_id_if("gpo-update-box", "hidden", gpo === null || !gpo.pending);
 }
 
 function apply_gpo_update() {
